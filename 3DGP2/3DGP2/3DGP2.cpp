3590ee491637cd315c9ea1553f57fc4655db6584
Lim Sooyoung <djwjod@naver.com>
2025-11-16 22:14:03 +0900

    코드 정리

--- 3DGP2/3DGP2/3DGP2.cpp
+++ 3DGP2/3DGP2/3DGP2.cpp
@@ -68,7 +68,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY3DGP2));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_MY3DGP2);
+    wcex.lpszMenuName = NULL;
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
